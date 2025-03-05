f244bb7034ba40a1ec86fe30fd32aef664c60749
Leo Romanovsky <leo@geteppo.com>
2025-03-05 13:02:20 -0800

    redudant

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -6,22 +6,3 @@ const String precomputedFlagsEndpoint = '/assignments';
 
 /// Default request timeout in milliseconds
 const int defaultRequestTimeoutMs = 5000;
-
-/// SDK platform enum
-enum SdkPlatform {
-  dart,
-  flutter,
-  other;
-
-  @override
-  String toString() {
-    switch (this) {
-      case SdkPlatform.dart:
-        return 'dart';
-      case SdkPlatform.flutter:
-        return 'flutter';
-      case SdkPlatform.other:
-        return 'other';
-    }
-  }
-}
