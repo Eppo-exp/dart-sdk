5c560b0c9e7c8608ac18d3e5b05ae873632ab247
Leo Romanovsky <leo@geteppo.com>
2025-03-13 13:33:27 -0700

    dart consistent with other SDKs

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,8 +1,8 @@
 /// Default base URL for Eppo API
-const String eppoBaseUrl = 'https://fscdn.eppo.cloud';
+const String eppoBaseUrl = 'https://fscdn.eppo.cloud/edge';
 
 /// Precomputed flags endpoint for fetching assignments
-const String precomputedFlagsEndpoint = '/edge/assignments';
+const String precomputedFlagsEndpoint = '/assignments';
 
 /// Default request timeout in milliseconds
 const Duration defaultRequestTimeout = Duration(seconds: 2);
